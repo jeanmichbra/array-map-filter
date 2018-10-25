229806ea52284d87ad74bffd0b513fd56e6eff66
jean-michel bravo <lcjmb@hotmail.fr>
2018-10-25 14:00:57 +0200

    Filter and add the positive numbers of filter / ex2.js.

--- filter/ex2.js
+++ filter/ex2.js
@@ -1,13 +1,17 @@
 /* Array.prototype.filter - Exercice 2
 
-Ecrire une fonction getMultiplesOf qui reçoit deux arguments:
+Ecrire une fonction getMultiplesOf qui reçoit
+ deux arguments:
 1. un tableau de nombres.
 2. un nombre.
-En utilisant filter, elle doit renvoyer un tableau ne contenant que les
-nombres du 1er argument qui sont multiples du nombre passé en 2ème.
+En utilisant filter, elle doit renvoyer 
+un tableau ne contenant que les
+nombres du 1er argument qui sont 
+multiples du nombre passé en 2ème.
 
 Exemple d'entrée:
-  1er argument:  [-12, -10, -7, -1, 5, 8, 11, 15, 20, 27]
+  1er argument:  
+  [-12, -10, -7, -1, 5, 8, 11, 15, 20, 27]
   2ème argument: 5
 
   Sortie attendue:
@@ -16,7 +20,20 @@ Exemple d'entrée:
  */
 
 function getMultiplesOf(numbers, n) {
-}
+  let getNumbers = [];
+  numbers.filter((multipleNumbers)=>{
+    if(multipleNumbers%5 === 0){
+      getNumbers.push(multipleNumbers);
+      return;
+    }else{
+      return;
+    }
 
+    
+  });
+  return getNumbers;
+}
+console.log(getMultiplesOf ([
+  -12, -10, -7, -1, 5, 8, 11, 15, 20, 27]));
 // Ne pas modifier l'export
 module.exports = getMultiplesOf;
