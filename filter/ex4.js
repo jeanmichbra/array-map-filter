177be9907c21dee8b65e92dc7ac47b78cd1c3b98
jean-michel bravo <lcjmb@hotmail.fr>
2018-10-26 11:20:33 +0200

    Work on filter prices ex4.js.

--- filter/ex4.js
+++ filter/ex4.js
@@ -1,12 +1,15 @@
 /* Array.prototype.filter - Exercice 4
 
-Ecrire une fonction filterOnPrice, qui attend 2 arguments:
-1. Un tableau d'objets représentant des produits, chacun ayant deux propriétés:
+Ecrire une fonction filterOnPrice, 
+qui attend 2 arguments:
+1. Un tableau d'objets représentant des produits, 
+chacun ayant deux propriétés:
   - name
   - price
 2. Un prix maximum
 
-La fonction doit renvoyer un tableau ne contenant que les produits ne dépassant pas le prix
+La fonction doit renvoyer un tableau ne contenant 
+que les produits ne dépassant pas le prix
 maximum.
 
 Exemple d'entrée:
@@ -28,8 +31,21 @@ Sortie attendue:
   ]
  */
 
-function filterOnPrice(products, maxPrice) {
+const filterOnPrice = (products, maxPrice) => {
+  const smallPricePhone = products.filter((minPrice) =>{
+
+    return minPrice.price <= maxPrice;
+  });
+  return smallPricePhone;
+
 }
 
+/*console.log(filterOnPrice ([
+  { name: 'iPhone X', price: 1159 },
+  { name: 'Xiaomi Mi A2', price: 209 },
+  { name: 'Samsung Galaxy Note 9', price: 992 },
+  { name: 'Huawei P20', price: 480 },
+  { name: 'Huawei P20 Pro', price: 649 }
+], 500));*/
 // Ne pas modifier l'export
 module.exports = filterOnPrice;
